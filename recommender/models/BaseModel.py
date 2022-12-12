a7ab84f4782ebd2258435c36454b78ae5a80a512
Jiawei He <45532786+JiaweiHe98@users.noreply.github.com>
2022-12-12 16:51:40 -0500

    base model grpc

--- recommender/models/BaseModel.py
+++ recommender/models/BaseModel.py
@@ -1,35 +1,64 @@
+import numpy as np
 import pandas as pd
 import pickle
 # from sklearn.neighbors import KNeighborsClassifier
 import os.path
 
+NONE_FEATURE = 3
+
 
 class BaseModel:
 
     def __init__(self) -> None:
-        if not os.path.exists('./assets/knn.pkl'):
+        if not os.path.exists('./assets/neigh.pkl'):
             raise RuntimeError('No model')
 
         if not os.path.exists('./assets/rest_final.csv'):
             raise RuntimeError('No data')
 
-        self.data = pd.read_csv('./assets/rest_final.csv', index_col=0)
+        self.data = pd.read_csv('./assets/rest_final.csv')
+        self.columns = len(self.data.columns) - NONE_FEATURE
+
         self.id_to_idx = {id: idx for idx,
                           id in enumerate(self.data['business_id'])}
 
-        with open('./assets/knn.pkl', 'rb') as f:
+        with open('./assets/neigh.pkl', 'rb') as f:
             self.model = pickle.load(f)
 
-    def recommend_one(self, business_id, length):
+    def recommend(self, business_ids, received, asking):
+        dummys = np.zeros((len(business_ids), self.columns))
 
-        dummy = self.data.iloc[self.id_to_idx[business_id]:, :-3]
-        recommendations = self.model.kneighbors(dummy, length)
-        print(recommendations[0].shape)
+        for idx, id in enumerate(business_ids):
 
-        # self.model.kneighbors()
+            dummys[idx] = self.data.iloc[self.id_to_idx[id], :-3].values.flatten()
 
-    # def recommend(bussiness_id):
+        distances, idxs = self.model.kneighbors(np.array(dummys))
+
+        visited_idx = set([self.id_to_idx[id] for id in business_ids])
+        res = {}
+
+        for i in range(len(business_ids)):
+            cur_dist = distances[i]
+            cur_idxs = idxs[i]
+
+            for j in range(len(cur_dist)):
+
+                if cur_idxs[j] in visited_idx:
+                    continue
+
+                if cur_idxs[j] not in res:
+                    res[cur_idxs[j]] = []
+
+                res[cur_idxs[j]].append(cur_dist[j])
+
+        res_final = sorted([(key, sum(lst) / len(lst))
+                           for key, lst in res.items()], key=lambda each: each[1])
+        all_ids = [self.data.iloc[idx, -3] for idx, _ in res_final]
+        return all_ids[min(received, len(all_ids) - 1): min(len(all_ids), received + asking)]
 
 
-model = BaseModel()
-model.recommend_one('MTSW4McQd7CbVtyjqoe9mw', 20)
+if __name__ == '__main__':
+    # for test
+    model = BaseModel()
+    print(model.recommend(['WnT9NIzQgLlILjPT0kEcsQ',
+                           'ENhzvzEbixDwE3f5G-shbg', '2o4CRQ-FreATtbnhlLKWew'], 0, 10))
--- recommender/rpc_server.py
+++ recommender/rpc_server.py
@@ -1,32 +1,57 @@
-from concurrent import futures
-import logging
-import grpc
 import os
 import sys
+import logging
+
+import grpc
+import RestaurantRecommendation_pb2_grpc
+import RestaurantRecommendation_pb2
+from concurrent import futures
+
+# from models.FinalModel import recommand_process
+from models.BaseModel import BaseModel
+
+PORT = 1080
+
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
-import RestaurantRecommendation_pb2
-import RestaurantRecommendation_pb2_grpc
-import sys 
-sys.path.append("..") 
-from models.FinalModel import recommand_process
+sys.path.append("..")
+
+
+class ModelService:
+    def __init__(self) -> None:
+        self.baseModel = BaseModel()
+
+    def recommendBaseModel(self, business_ids, received, asking):
+        return self.baseModel.recommend(business_ids, received, asking)
+
 
 class RestaurantRecommendationService(RestaurantRecommendation_pb2_grpc.RestaurantRecommendationService):
+
+    def __init__(self) -> None:
+        self.modelService = ModelService()
+
     def getBaseModelRecommendation(self, request, context):
-        pass
+        ids = self.modelService.recommendBaseModel(
+            request.ids, request.received, request.asking)
+        return RestaurantRecommendation_pb2.RecommendedRestaurants(ids=ids)
+
     def getFinalModelRecommendation(self, request, context):
-        result_ids = recommand_process(request)
-        return RestaurantRecommendation_pb2.RecommendedRestaurants(ids = result_ids)
+        # result_ids = recommand_process(request)
+        # return RestaurantRecommendation_pb2.RecommendedRestaurants(ids=result_ids)
+        pass
+
 
 def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=2))
     RestaurantRecommendation_pb2_grpc.add_RestaurantRecommendationServiceServicer_to_server(
         RestaurantRecommendationService(), server
     )
-    server.add_insecure_port('[::]:50052')
+    server.add_insecure_port(f'[::]:{PORT}')
     server.start()
-    print("Listening on port 10052")
+    print(f"Listening on port {PORT}")
     server.wait_for_termination()
+
 
 if __name__ == '__main__':
     print('Server starts!')
